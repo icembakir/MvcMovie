0a9c3b80b145d7e39e38c6b0e50e7a9fbaa2779a
Cem <icembakir@gmail.com>
2018-08-28 12:56:34 +0300

    Cshtml files - create, delete, details, edit, index- were added with controller.

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -4,6 +4,7 @@ using System.EnterpriseServices.Internal;
 using System.Linq;
 using System.Web;
 using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
 
 namespace MvcMovie.Models
 {
@@ -11,12 +12,15 @@ namespace MvcMovie.Models
     {
         public int ID { get; set; }
         public string Title { get; set; }
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
         public decimal Price { get; set; }
     }
 
-    public class MovieDBContext : DbContext
+    public class MovieDbContext : DbContext
     {
         public DbSet<Movie> Movies { get; set; }
     }
